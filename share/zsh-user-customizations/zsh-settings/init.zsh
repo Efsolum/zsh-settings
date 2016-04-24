19e5521f4608c85fdcdaa8f5b7f3e194c5ef107c
Matthew Jordan <matthew@technidragon.Home>
2016-04-24 11:47:53 -0400

    Moved aliases from main config.

--- share/zsh-user-customizations/zsh-settings/init.zsh
+++ share/zsh-user-customizations/zsh-settings/init.zsh
@@ -3,6 +3,7 @@ package_dir=$(dirname $_)
 source $package_dir/autoloads.zsh
 source $package_dir/keybindings.zsh
 source $package_dir/options.zsh
+source $package_dir/aliases.zsh
 
 export HISTSIZE=100000
 export SAVEHIST=$HISTSIZE
