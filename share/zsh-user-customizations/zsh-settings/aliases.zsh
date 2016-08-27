913605abdfd0875da47fdb3e6a950953d1779a0b
Matthew Jordan <matthewjordandevops@yandex.com>
2016-08-27 12:07:31 -0400

    Added condistion to some aliases.

--- share/zsh-user-customizations/zsh-settings/aliases.zsh
+++ share/zsh-user-customizations/zsh-settings/aliases.zsh
@@ -8,10 +8,12 @@ alias lpe=load_project_env
 alias npd=new_project_dir
 
 # Flash/Portable drive related aliases
-alias uds="udisksctl status"
-alias udm="udisksctl mount --block-device"
-alias udu="udisksctl unmount --block-device"
-alias udi="udisksctl info --block-device"
+if [[ -f $(type udisksctl) ]]; then
+		alias uds="udisksctl status"
+		alias udm="udisksctl mount --block-device"
+		alias udu="udisksctl unmount --block-device"
+		alias udi="udisksctl info --block-device"
+fi
 
 #miscellaneous aliases
 alias noblank='xset -dpms; xset s off'
@@ -22,12 +24,14 @@ alias rzsh='exec env -i LANG=$LANG zsh -li'
 alias rpane='tmux respawn-pane -k'
 
 # APT aliases
-alias apts='apt-cache search'
-alias aptf='apt-file search'
-alias aptu="print 'apt-get update && apt-file update' | sudo zsh -s"
-alias apti='sudo apt-get install'
-alias aptp='apt-cache policy'
-alias aptm='apt-cache madison'
+if [[ -f $(type apt-cache) ]]; then
+		alias apts='apt-cache search'
+		alias aptf='apt-file search'
+		alias aptu="print 'apt-get update && apt-file update' | sudo zsh -s"
+		alias apti='sudo apt-get install'
+		alias aptp='apt-cache policy'
+		alias aptm='apt-cache madison'
+fi
 
 # SSH aliases
 alias ssha=ssh-add
@@ -36,7 +40,9 @@ alias sshx="ssh-add -x"
 alias sshX="ssh-add -X"
 
 # Sync aliases
-alias syncpersonal="unison personal -batch; unison personal"
-alias syncprojects="unison projects -batch; unison projects"
-alias syncarchived="unison archived -batch; unison archived"
-alias syncstow="unison stow -batch; unison stow"
+if [[ -f $(type unison) ]]; then
+		alias syncpersonal="unison personal -batch; unison personal"
+		alias syncprojects="unison projects -batch; unison projects"
+		alias syncarchived="unison archived -batch; unison archived"
+		alias syncstow="unison stow -batch; unison stow"
+fi
